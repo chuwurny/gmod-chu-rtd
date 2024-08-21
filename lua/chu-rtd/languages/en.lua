385aa99ce08c91bff108d3c198df596f61f5e430
chuwurny <chuwurny@national.shitposting.agency>
2024-08-21 23:43:04 +0300

    Add english language

---
+++ lua/chu-rtd/languages/en.lua
@@ -0,0 +1,59 @@
+local lang = chuRtd.LanguageContext:DefineLanguage("en")
+
+lang:Phrase("rolled-effect", "rolled effect")
+lang:Phrase("with-duration", "with duration")
+lang:Phrase("seconds", "sec.")
+lang:Phrase("effect-ended", "effect ended")
+lang:Phrase("died-with-active-effect", "died with active effect")
+
+lang:Phrase("effect.godmode.name", "Godmode")
+lang:Phrase("effect.fast-speed.name", "Fast speed")
+lang:Phrase("effect.noclip.name", "Noclip")
+lang:Phrase("effect.low-gravity.name", "Low gravity")
+lang:Phrase("effect.invisibility.name", "Invisibility")
+lang:Phrase("effect.increased-damage.name", "Increased damage")
+lang:Phrase("effect.infinity-ammo.name", "Infinity ammo")
+lang:Phrase("effect.freezing-bullets.name", "Freezing bullets")
+lang:Phrase("effect.drug-bullets.name", "Drug bullets")
+lang:Phrase("effect.dissolving-bullets.name", "Dissolving bullets")
+lang:Phrase("effect.death.name", "Death")
+lang:Phrase("effect.dissolve.name", "Dissolve")
+lang:Phrase("effect.snail.name", "Snail")
+lang:Phrase("effect.freeze.name", "Freeze")
+lang:Phrase("effect.1hp.name", "1 HP")
+lang:Phrase("effect.drunk.name", "Drunk")
+lang:Phrase("effect.blindness.name", "Blindness")
+
+lang:Phrase("effect.lets-build.name", "The true builder")
+lang:Phrase("effect.lets-build.oh-i-love-building", "Oh, I love building!")
+lang:Phrase("effect.lets-build.bob-the-builder-can-we-fix-it",
+            "Booob the builder, can we fix it, booob the builder, yes we can!")
+lang:Phrase("effect.lets-build.bought-gmod-to-build",
+            "I bought gmod to build")
+lang:Phrase("effect.lets-build.i-hate-pvp-lets-build",
+            "Fuck PvP, let's build guys!")
+lang:Phrase("effect.lets-build.im-gonna-build-huge-d-i-mean-house",
+            "I'm gonna build huge d... I mean house!")
+
+lang:Phrase("effect.beacon.name", "Beacon")
+lang:Phrase("effect.179fov.name", "Strange feeling")
+lang:Phrase("effect.force-field.name", "Force field")
+lang:Phrase("effect.strong-gravity.name", "Strong gravity")
+lang:Phrase("effect.inside-prop.name", "I Am Prop")
+lang:Phrase("effect.eye-for-an-eye.name", "Eye of an eye")
+lang:Phrase("effect.its-raining-nades.name", "It's raining men! Hallelujah!")
+lang:Phrase("effect.target-me.name", "I'm special")
+lang:Phrase("effect.x2hp.name", "Double health")
+lang:Phrase("effect.no-gravity.name", "Cosmonaut")
+lang:Phrase("effect.magnet.name", "Magnet")
+lang:Phrase("effect.strong-recoil.name", "Strong recoil")
+lang:Phrase("effect.no-step.name", "Incline problems")
+lang:Phrase("effect.lags.name", "L-lags")
+
+lang:Phrase("effect.prop-madness.name", "Prop madness")
+lang:Phrase("effect.prop-madness.crouch-to-fly-props-at-aimpos",
+            "Crouch to fly props at aim position")
+
+lang:Phrase("effect.head-roll.name", "Head roll")
+lang:Phrase("effect.dizzy.name", "Dizzy")
+lang:Phrase("effect.strip-weapons.name", "Strip weapons")
