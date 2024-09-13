3912bc46e001d1c44e216deabd85285c5749875f
chuwurny <chuwurny@national.shitposting.agency>
2024-09-13 04:09:14 +0300

    Add helper function FindNearestPlayer

---
+++ lua/chu-rtd/helpers/sh_find.lua
@@ -0,0 +1,40 @@
+function chuRtd.Helpers.FindNearestPlayer(origin, filter, maxDistance)
+    if filter then
+        if type(filter) == "table" then
+            local filterSet = x.SetFromSequence(filter)
+
+            filter = function(ent)
+                return not filterSet:Has(ent)
+            end
+        elseif isplayer(filter) then
+            local filterEnt = filter
+
+            filter = function(ent)
+                return ent ~= filterEnt
+            end
+        end
+    end
+
+    local target = nil
+    local targetDist = math.huge
+
+    for _, ply in player.Iterator() do
+        local dist = ply:GetPos():Distance(origin)
+
+        if
+            -- check if distance is within max distance (if set)
+            (not maxDistance or dist < maxDistance) and
+            -- check if ply is closer to origin
+            targetDist > dist and
+            -- check if it's not filtered (if set)
+            (not filter or filter(ply)) and
+            -- check if player is alive
+            ply:Alive()
+        then
+            target = ply
+            targetDist = dist
+        end
+    end
+
+    return target, targetDist
+end
