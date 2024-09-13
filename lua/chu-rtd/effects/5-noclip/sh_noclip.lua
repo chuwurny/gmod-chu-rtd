9fb7e918941c633f6c263eea25e0b40ab1c58253
chuwurny <chuwurny@national.shitposting.agency>
2024-09-13 15:58:58 +0300

    Allow to toggle noclip

--- lua/chu-rtd/effects/5-noclip/sh_noclip.lua
+++ lua/chu-rtd/effects/5-noclip/sh_noclip.lua
@@ -1,1 +1,10 @@
-chuRtd.Effect("noclip", chuRtd.COLOR_GOOD)
+local effect = chuRtd.Effect("noclip", chuRtd.COLOR_GOOD)
+
+effect:Hook("PlayerNoclip", function(ply, state)
+    if not ply.RtdData then return end
+    if ply:GetRtdEffect() ~= effect then return end
+
+    if state then
+        return true
+    end
+end, HOOK_MONITOR_HIGH)
