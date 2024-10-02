5c2c2e106aa2b64b9eb9121dde81305d9c17fee4
chuwurny <chuwurny@national.shitposting.agency>
2024-10-03 02:24:21 +0300

    Fix gravity effect

--- lua/chu-rtd/effects/48-no-gravity/sv_strong-gravity.lua
+++ lua/chu-rtd/effects/48-no-gravity/sv_strong-gravity.lua
@@ -3,7 +3,8 @@ local effect = chuRtd.Effects:Get("no-gravity")
 function effect:OnRolled(ply, data)
     data.oGravity = ply:GetGravity()
 
-    ply:SetGravity(0)
+    -- HACK: somehow after update value 0 doesn't work
+    ply:SetGravity(0.0001)
 end
 
 function effect:OnEnded(ply, data)
