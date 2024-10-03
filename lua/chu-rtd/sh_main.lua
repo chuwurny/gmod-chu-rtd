33393f6e242fd30d46c074fe9948054fe76f929b
chuwurny <chuwurny@national.shitposting.agency>
2024-10-03 18:16:13 +0300

    Fix effects are not synced

--- lua/chu-rtd/sh_main.lua
+++ lua/chu-rtd/sh_main.lua
@@ -1,3 +1,10 @@
 chuRtd = chuRtd or {}
 chuRtd.Helpers = chuRtd.Helpers or {}
 chuRtd.Effects = chuRtd.Effects or x.Map()
+
+-- Sort effects to ensure indices are same
+x.EnsureInitPostEntity(function()
+    chuRtd.Effects:Sort(function(a, b)
+        return a.Id > b.Id
+    end)
+end)
